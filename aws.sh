79b6d61c90eba478696aeee4e2154ec3e7361e01
prasadvamsi <33920006+prasadvamsi@users.noreply.github.com>
2018-03-22 12:36:24 +0530

    Delete aws.sh

--- aws.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-instance=`aws ec2 run-instances --image-id ami-66506c1c --count 1 --instance-type t2.micro --key-name "aws practice"|grep InstanceId|awk -F: '{print $2}'|awk -F, '{print $1}'|tr -d '"'|tr -s ""`
-sleep 100
-ip=`aws ec2 describe-instances --instance-ids $instance|grep PublicIpAddress|awk -F: '{print $2}'|awk -F, '{print $1}'|tr -d '"'|awk '{print $1}'`
-
-ssh -i /root/awspractice.pem ec2-user@$ip -o StrictHostKeyChecking=no
